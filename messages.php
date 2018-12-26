7b2fd23822ac51bc460890d30758e5a9ea70b103
anjey58 <korochkovam@mail.ru>
2018-12-26 14:09:36 +0300

    Add files via upload

---
+++ messages.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'Settings XAdmin' => 'Настройки XAdmin',
+    'Editing the url-address login of the admin panel' => 'Редактирование url-адреса входа в панель администратора',
+    'Managing the url-address login of admin panel' => 'Управление url-адресом входа в панель администратора',
+    'Manages access of edit the url-address login of the admin panel' => 'Управляет доступом редактирования url-адреса входа в панель администратора',
+    'Refresh' => 'Обновить',
+    'Url-address' => 'Url-адрес',
+    'Resulting url-address' => 'Результирующий url-адрес',
+    'Enter the url-address of the login to the admin panel' => 'Введите url-адрес входа в панель администратора',
+    'Fill in the "Url-address", valid characters: a-z, 0-9' => 'Заполните поле "Url-адрес", допустимые символы: a-z, 0-9',
+    'Url-address loaded successfully' => 'Url-адрес загружен успешно',
+    'Url-address saved successfully' => 'Url-адрес сохранен успешно',
+];
